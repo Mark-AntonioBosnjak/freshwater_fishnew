08a5261892258e5e61902868069c24b0a077af91
Mark-Antonio Bosnjak <Mark-Antonio.Bosnjak@netcetera.com>
2022-09-30 13:49:26 +0200

    Initial commit

---
+++ freshwater_fish/freshwater_fish/src/main/java/ch/bbw/m151/freshwater_fish/FreshwaterFishApplication.java
@@ -0,0 +1,13 @@
+package ch.bbw.m151.freshwater_fish;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FreshwaterFishApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FreshwaterFishApplication.class, args);
+	}
+
+}
